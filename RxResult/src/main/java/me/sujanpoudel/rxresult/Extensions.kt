ac6ccdbfa2d9d3199914d48cd1e0dcff12ac443a
psuzn <mail@sujanpoudel.me>
2020-08-22 23:26:36 +0545

    @CheckResult annotation added

--- RxResult/src/main/java/me/sujanpoudel/rxresult/Extensions.kt
+++ RxResult/src/main/java/me/sujanpoudel/rxresult/Extensions.kt
@@ -1,25 +1,30 @@
 package me.sujanpoudel.rxresult
 
 import android.content.Intent
+import androidx.annotation.CheckResult
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import io.reactivex.Observable
 import io.reactivex.Single
 
+
+@CheckResult
 fun <T> AppCompatActivity.startForResult(intent: Intent, resultExtractor: (Intent) -> T): Single<T> {
     return ActivityResultSingle(this, intent, resultExtractor)
 }
 
+@CheckResult
 fun AppCompatActivity.startForResult(intent: Intent) = startForResult(intent) { it }
 
-
+@CheckResult
 fun <T> Fragment.startForResult(intent: Intent, resultExtractor: (Intent) -> T): Single<T> {
     return ActivityResultSingle(this, intent, resultExtractor)
 }
 
+@CheckResult
 fun Fragment.startForResult(intent: Intent) = startForResult(intent) { it }
 
-
+@CheckResult
 fun <T> AppCompatActivity.startForResult(intent: Observable<Intent>, resultExtractor: (Intent) -> T): Observable<T> {
     return intent
         .flatMap {
@@ -27,13 +32,15 @@ fun <T> AppCompatActivity.startForResult(intent: Observable<Intent>, resultExtra
         }
 }
 
+@CheckResult
 fun AppCompatActivity.startForResult(intent: Observable<Intent>) = startForResult(intent) { it }
 
-
+@CheckResult
 fun <T> Fragment.startForResult(intent: Observable<Intent>, resultExtractor: (Intent) -> T): Observable<T> {
     return intent.flatMap {
         ActivityResultSingle(this, it, resultExtractor).toObservable()
     }
 }
 
+@CheckResult
 fun Fragment.startForResult(intent: Observable<Intent>) = startForResult(intent) { it }
