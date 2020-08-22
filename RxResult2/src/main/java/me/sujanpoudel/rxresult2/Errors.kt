8c0232287b6a246f36af7e738df0009c3e8cff3e
psuzn <mail@sujanpoudel.me>
2020-08-22 22:16:40 +0545

    initial commit

---
+++ RxResult2/src/main/java/me/sujanpoudel/rxresult2/Errors.kt
@@ -0,0 +1,3 @@
+package me.sujanpoudel.rxresult2
+
+object ActivityNullResultException : Throwable()
---
+++ RxResult2/src/main/java/me/sujanpoudel/rxresult2/Extensions.kt
@@ -0,0 +1,39 @@
+package me.sujanpoudel.rxresult2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import io.reactivex.Observable
+import io.reactivex.Single
+
+fun <T> AppCompatActivity.startForResult(intent: Intent, resultExtractor: (Intent) -> T): Single<T> {
+    return ActivityResultSingle(this, intent, resultExtractor)
+}
+
+fun AppCompatActivity.startForResult(intent: Intent) = startForResult(intent) { it }
+
+
+fun <T> Fragment.startForResult(intent: Intent, resultExtractor: (Intent) -> T): Single<T> {
+    return ActivityResultSingle(this, intent, resultExtractor)
+}
+
+fun Fragment.startForResult(intent: Intent) = startForResult(intent) { it }
+
+
+fun <T> AppCompatActivity.startForResult(intent: Observable<Intent>, resultExtractor: (Intent) -> T): Observable<T> {
+    return intent
+        .flatMap {
+            ActivityResultSingle(this, it, resultExtractor).toObservable()
+        }
+}
+
+fun AppCompatActivity.startForResult(intent: Observable<Intent>) = startForResult(intent) { it }
+
+
+fun <T> Fragment.startForResult(intent: Observable<Intent>, resultExtractor: (Intent) -> T): Observable<T> {
+    return intent.flatMap {
+        ActivityResultSingle(this, it, resultExtractor).toObservable()
+    }
+}
+
+fun Fragment.startForResult(intent: Observable<Intent>) = startForResult(intent) { it }
